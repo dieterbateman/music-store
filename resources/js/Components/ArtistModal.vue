b5ce28bef6f38be7e625e8fdfd8c0269aeadccf6
Dieter Bateman <batemandieter@gmail.com>
2022-10-21 22:02:08 +0200

    Just playing around

--- resources/js/Components/ArtistModal.vue
+++ resources/js/Components/ArtistModal.vue
@@ -124,7 +124,7 @@ export default {
             form.name = null;
         });
         function destroyArtist(id) {
-            if (confirm("Are you sure?")) {
+            if (confirm("Are you sure you want to partake in cancel culture?")) {
                 Inertia.delete(route("artists.destroy", id));
             }
         }
